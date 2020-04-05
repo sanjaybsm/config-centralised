d92391c96338ad142584a6b6aa34dd0f70db65d7
sanjaybsm <sanjaybsm@gmail.com>
2020-04-05 12:18:25 +0200

    Initial commit

---
+++ src/test/java/com/example/configcentralised/ConfigCentralisedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configcentralised;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCentralisedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
